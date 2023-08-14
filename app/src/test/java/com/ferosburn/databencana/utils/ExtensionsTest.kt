591b11d90751033c2ba63fa267d02603eb412590
Su'udi Khoirul Anam <udi.khoirul@gmail.com>
2023-08-14 23:35:47 +0700

    refactor: change test function name

--- app/src/test/java/com/ferosburn/databencana/utils/ExtensionsTest.kt
+++ app/src/test/java/com/ferosburn/databencana/utils/ExtensionsTest.kt
@@ -2,39 +2,39 @@ package com.ferosburn.databencana.utils
 
 import com.ferosburn.databencana.domain.model.DisasterTypes
 import com.ferosburn.databencana.domain.model.Provinces
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ExtensionsTest {
 
     @Test
-    fun provinceNameToProvincesTest() {
+    fun testProvinceNameToProvinces() {
         val provinceName = "Banten"
-        Assert.assertEquals(Provinces.BANTEN, provinceName.provinceNameToProvinces())
+        assertEquals(Provinces.BANTEN, provinceName.provinceNameToProvinces())
     }
 
     @Test
-    fun provinceCodeToProvincesTest() {
+    fun testProvinceCodeToProvinces() {
         val provinceCode = "ID-BT"
-        Assert.assertEquals(Provinces.BANTEN, provinceCode.provinceCodeToProvinces())
+        assertEquals(Provinces.BANTEN, provinceCode.provinceCodeToProvinces())
     }
 
     @Test
-    fun disasterValueToDisasterTypesTest() {
+    fun testDisasterValueToDisasterTypes() {
         val disasterValue = "fire"
-        Assert.assertEquals(DisasterTypes.FIRE, disasterValue.disasterValueToDisasterTypes())
+        assertEquals(DisasterTypes.FIRE, disasterValue.disasterValueToDisasterTypes())
     }
 
     @Test
-    fun disasterNameToDisasterTypesTest() {
+    fun testDisasterNameToDisasterTypes() {
         val disasterName = "Kebakaran"
-        Assert.assertEquals(DisasterTypes.FIRE, disasterName.disasterNameToDisasterTypes())
+        assertEquals(DisasterTypes.FIRE, disasterName.disasterNameToDisasterTypes())
     }
 
     @Test
-    fun localDateToFormattedDateTimeTest() {
+    fun testLocalDateToFormattedDateTime() {
         val localDate = "20-12-2012"
-        Assert.assertEquals("2012-12-20T00:00:00+0000", localDate.localDateToFormattedDateTime("dd-MM-yyyy"))
-        Assert.assertEquals(null, "".localDateToFormattedDateTime("dd-MM-yyyy"))
+        assertEquals("2012-12-20T00:00:00+0000", localDate.localDateToFormattedDateTime("dd-MM-yyyy"))
+        assertEquals(null, "".localDateToFormattedDateTime("dd-MM-yyyy"))
     }
 }
